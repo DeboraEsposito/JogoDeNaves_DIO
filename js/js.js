fdeadcfbd7e821e5183270c6e9904666a2530fa7
DeboraEsposito <85322183+DeboraEsposito@users.noreply.github.com>
2022-02-22 19:08:46 -0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -455,7 +455,7 @@ function gameOver() {
   
   $("#fundoGame").append("<div id='fim'></div>");
   
-  $("#fim").html("<h1> Game Over </h1><p>Sua pontuaзгo foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
+  $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
 } // Fim da função gameOver();
 } // Fim da função start
 
